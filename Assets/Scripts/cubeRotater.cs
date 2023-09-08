a37e6dea2fd7d9cdc311722ee8b9920b233cc40c
Chris Schnurr <ctschnurr@hotmail.com>
2023-09-08 08:16:15 -0300

    Initial commit

---
+++ Assets/Scripts/cubeRotater.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeRotater : MonoBehaviour
+{
+
+    GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cube.transform.Rotate(0, 1, 0);
+    }
+}
